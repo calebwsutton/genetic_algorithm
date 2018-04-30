44801a6b11580d89a51b60f3e234425395efff22
Caleb <calsutton728@gmail.com>
2018-04-30 17:40:32 -0500

    first working commit, output seems logical, hangs completely for large populations

--- genetic.py
+++ genetic.py
@@ -34,23 +34,51 @@ import random
 #
 def artificial_selection(population, puzzle, num_generations, pop_size):
 
-	#  for i in range(num_generations):
-	#       for i in range(population_size/2):
-	#            population.append(crossover(select_parents(population))) # this will probably throw an error, can you pass arguments as a tuple??
-	#       while(len(population) != pop_size) # this is definetly throwing an error
-	#            kill_unfit(population)
+	for i in range(num_generations):
+		#print('Generation: ' + str(i))
+		for _ in range(pop_size//2): # increase population by 50%
+			parent1, parent2 = select_parents(population)
+			#print(parent1)
+			population.append(crossover(parent1, parent2, puzzle))
+		while len(population) != pop_size:
+			kill_unfit(population)
 
 	# determine most fit chomosome
+	most_fit = population[0]
+
+	for chromosome in population:
+		if chromosome['fitness'] < most_fit['fitness']:
+			most_fit = chromosome
+
 
 	return most_fit
 
 
-# Function which chooses a chromosome using the ***selection mechanism***
-# and removes it from the population
+# Function which chooses a chromosome using the roulette wheel
+# selection mechanism and removes it from the population
 def kill_unfit(population):
-	 total_cost = 0
+	total_cost = 0
+	selection_pool = []
 
-	 return True
+	random.seed()
+
+	for chrom in population:
+		total_cost += chrom['fitness']
+
+	previous_chance = 0
+	for chrom in population:
+		chance = chrom['fitness'] / total_cost
+		selection_pool.append({'chance': chance, 'chromosome': chrom, 'window': [previous_chance, previous_chance + chance]})
+
+		previous_chance += chance
+
+	selector = random.uniform(0, 1)
+
+	for chrom in selection_pool:
+		if selector >= chrom['window'][0] and selector < chrom['window'][1]:
+			population.remove(chrom['chromosome'])
+
+	return True
 
 
 
@@ -70,32 +98,38 @@ def select_parents(population):
 	previous_chance = 0
 	for chrom in population:
 		chance = (1/chrom['fitness']) / total_fitness # this is the percentage chance each chromosome will be selected
-		selection_pool.append({'chance': chance, 'chromosome': chrom,'window': [previous_chance, previous_chance + chance]})    # each chromosome is given a window to get selected that lies somewhere in the range of 0 - 1. 
+		selection_pool.append({'chance': chance, 'chromosome': chrom,'window': [previous_chance, previous_chance + chance]})	# each chromosome is given a window to get selected that lies somewhere in the range of 0 - 1.
 																																# The windows should not overlap and each should be proportional to the chance of getting
 																																# selected, so that when a random float is generated bewteen 0 - 1 it will fall into the window of
 																																# one of the chromosomes in the selection pool.
 
 																																# ie one might be 0.0 - 0.2, the next might be 0.2 - 0.21, then 0.21-0.7, etc......
-																																
+
 		previous_chance += chance
 
 
 	selector1 = random.uniform(0, 1)    # generate a random number that will choose a parent
-	selector2 = random.uniform(0, 1) 
+	selector2 = random.uniform(0, 1)
 
-	# 
+
 	for chrom in selection_pool:
 		if selector1 > chrom['window'][0] and selector1 < chrom['window'][1]:
-			while selector2 > chrom['window'][0] and selector2 < chrom['window'][1]:    # as long as the second selector fall with in the selection of range of the first parent generate a new random selector
+			#print('MATCHED 1!: ' + str(chrom['chromosome']))
+			while selector2 >= chrom['window'][0] and selector2 < chrom['window'][1]:    # as long as the second selector fall with in the selection of range of the first parent generate a new random selector
 																						# the point of this is so both of the parents arent the same chromosome
-				selector2 = random.uniform(0,1)
-			parent1 = selection_pool.pop(chrom)
+				selector2 = random.uniform(0, 1)
+			parent1 = chrom['chromosome']
+			selection_pool.remove(chrom)
 
 	for chrom in selection_pool:
-		if selector2 > chrom['window'][0] and selector2 < chrom['window'][1]:
-			parent2 = selection_pool.pop(chrom)
+		if selector2 >= chrom['window'][0] and selector2 < chrom['window'][1]:
+			#print('MATCHED 2!: ' + str(chrom['chromosome']))
+			parent2 = chrom['chromosome']
+			selection_pool.remove(chrom)
 
-
+	# print(parent1)
+	# print(parent2)
+	
 	return(parent1, parent2)
 
 
@@ -106,14 +140,23 @@ def select_parents(population):
 #
 # Note: need to be careful about bug which could produce
 #    a chromosome with consecutive 0s
-def crossover(parent1, parent2):
-	cross_point = random.randint(0, len(parent1['alleles'] - 2))
+def crossover(parent1, parent2, puzzle):
+	#Sprint(parent1)
+	cross_point = random.randint(0, len(parent1['alleles']) - 2)
+	offspring = {'alleles': [], 'fitness': 0}
 
-	while (parent1['alleles'][cross_point] == 0 and parent1['alleles'][cross_point + 1] == 0): # generate a new cross point as long as 
-		cross_point = random.randint(0, len(parent1['alleles'] - 2))
-	
-	offspring = parent1[:cross_point] + parent2[cross_point+1:]
+	while (parent1['alleles'][cross_point] == 0 and parent2['alleles'][cross_point + 1] == 0): # generate a new cross point as long as 
+		cross_point = random.randint(0, len(parent1['alleles']) - 2)
+
+	for i in range(len(parent1['alleles'])):
+		if i < cross_point + 1:
+			offspring['alleles'].append(parent1['alleles'][i])
+		else:
+			offspring['alleles'].append(parent2['alleles'][i])
+
 	mutate(offspring)
+
+	offspring['fitness'] = get_fitness(offspring['alleles'], puzzle)
 
 	return offspring
 
@@ -130,15 +173,14 @@ def mutate(chromosome):
 			if chromosome['alleles'][i] == 0:   # if the allele is a 0 we can just flip it to a 1 now worries
 				chromosome['alleles'][i] = 1
 				return_val = True
-			else:   # if it's a 1 we need to make sure the adjacent alleles are also 1 in order to flip to zero
+			else: # if it's a 1 we need to make sure the adjacent alleles are also 1 in order to flip to zero
 				if i == 0 and i + 1 < len(chromosome['alleles']):   # Case: first allele
 					if  chromosome['alleles'][i + 1] == 1:          # Only Check the next allele
 						chromosome['alleles'][i] = 0
 						return_val = True
-				elif i == len(chromosome['alleles']) - 1:           # Case: last allele
-					if chromosome['alleles'][i - 1] == 1:           # Only check the previous allele
-						chromosome['alleles'][i] = 0
-						return_val = True
+				elif i == len(chromosome['alleles']) - 1:           # Case: last allele cannot be 0
+					chromosome['alleles'][i] = 1
+					return_val = False
 				elif chromosome['alleles'][i + 1] == 1 and chromosome['alleles'][i - 1] == 1:   # Case: Somewhere in middle, check previous and next allele
 					chromosome['alleles'][i] = 0
 					return_val = True
@@ -152,7 +194,7 @@ def get_fitness(alleles, puzzle):
 	board = puzzle[1:]
 	fittness_count = 0
 
-	for i in range(len(alleles)):
+	for i in range(len(board)):
 		if alleles[i] == 1:
 			fittness_count += board[i]
 
@@ -172,9 +214,9 @@ def create_random_population(puzzle):
 	for length in range(pop_size):
 		chrom = {'alleles' : [], 'fitness': 0}
 		for i in range(pop_item_length):
-			bit = random.randint(0,1)
-			if (i > 0 and bit == 0):
-				if chrom[i-1] == 0:
+			bit = random.randint(0, 1)
+			if i > 0 and bit == 0:
+				if chrom['alleles'][i-1] == 0:
 					chrom['alleles'].append(1)
 				else:
 					chrom['alleles'].append(bit)
@@ -220,7 +262,7 @@ def print_data():
 # function which reads the input file and returns an array
 # of all the puzzles
 def read_data(input_file):
-	list_table =[]
+	list_table = []
 	try:
 		with open(input_file, "r") as data_file:
 			for line in data_file:
@@ -228,24 +270,25 @@ def read_data(input_file):
 				item = list(map(int, item))
 				list_table.append(item)
 
-		return (list_table)
+		return list_table
 
 	# Throw error if issue reading file.
 	except IOError:
-		print ("Error reading file.", input_file_name)
+		print("Error reading file.", input_file)
 		sys.exit()
 
 # function which calls artificial_selection() and dynamic_programming()
 # for each puzzle and prints the results to the console
 def main():
- 
-	 # for each puzzle call artificial_selection() and dynamic_programming()
-	 #    and print the results
+
+	# for each puzzle call artificial_selection() and dynamic_programming()
+	#    and print the results
 	if len(sys.argv) > 1:
 		input_file_name = sys.argv[1]                    # Accepts filename as cmd line argument
-		input_table = read_data(input_file_name) 
-		for line in input_table:
-			pop = create_random_population(line)
+		input_table = read_data(input_file_name)
+		for puzzle in input_table:
+			print('\nPuzzle:\t\t\t' + str(puzzle))
+			print('Genetic Solution:\t' + str([1] + artificial_selection(create_random_population(puzzle), puzzle, 25, 100)['alleles']))
 
 	else:
 		print ("Please enter the correct cmd line arguments in the format:")
